d60c478b55d45d3a70acad522ac64ab04235da37
OsmanSE <osmansalih717@gmail.com>
2018-12-23 17:21:15 -0500

    definition and math problems update

--- src/math/problems/Factorial.java
+++ src/math/problems/Factorial.java
@@ -11,6 +11,19 @@ public class Factorial {
          * Write a java program to find Factorial of a given number using Recursion as well as Iteration.
          *
          */
+        int fact=1;
+        int n=5;
+//Factorial using loop
+        for (int i=1;i<=n;i++){fact=fact*i;}
+        System.out.println("Factorial of "+n+ " is  : "+fact);
+    }
+    //factorial using recursion
 
+    public  int factorial(int x)
+    {
+        int result;
+        if(x==0) {return 1;}
+        else  result=factorial(x-1)*x;
+        return result;
     }
 }
--- src/math/problems/FindMissingNumber.java
+++ src/math/problems/FindMissingNumber.java
@@ -11,7 +11,23 @@ public class FindMissingNumber {
          * For example {10, 2, 1, 4, 5, 3, 7, 8, 6}. One number will be missing in array (9 in this case).
          * Write java code to find the missing number from the array. Write static helper method to find it.
          */
-         int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+        int [] array = new int[]{10, 2, 1, 4, 5, 3, 7, 8, 6};
+        System.out.println("The missing number is : "+missingNumber(array) );
 
     }
+    public static int missingNumber(int[] a){
+        int mis=0;
+        int total=0;
+        int sum=0;
+
+        for(int i=1;i<=a.length+1;i++)
+            total=total+i;
+
+        for (int j=0;j<a.length;j++)
+            sum=sum+a[j];
+
+        mis=total-sum;
+        return mis;
+    }
 }
+
