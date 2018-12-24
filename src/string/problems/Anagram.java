9d66f11d0ef1fc243da1e59d1661659d1d081582
OsmanSE <osmansalih717@gmail.com>
2018-12-23 20:32:42 -0500

    String problems Anagram

--- src/string/problems/Anagram.java
+++ src/string/problems/Anagram.java
@@ -1,5 +1,7 @@
 package string.problems;
 
+import java.util.Arrays;
+
 /**
  * Created by mrahman on 04/22/17.
  */
@@ -8,5 +10,28 @@ public class Anagram {
     public static void main(String[] args) {
         //Write a Java Program to check if the two String are Anagram. Two String are called Anagram when there is
         //same character but in different order.For example,"CAT" and "ACT", "ARMY" and "MARY".
+
+        Anagram an = new Anagram();
+        String s1 = "ARMY";
+        String s2 = "Mary";
+        System.out.println("Is " + s1 + " and " + s2 + " are anagram :" + an.anagram(s1, s2));
+
+    }
+
+    public boolean anagram(String s1, String s2) {
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        s1.toUpperCase();
+        s2.toUpperCase();
+        char[] c1 = s1.toCharArray();
+        char[] c2 = s2.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        return Arrays.equals(c1, c2);
+
+
     }
 }
