9795b525fa38667f74616b0b07eb4fd3650df280
OsmanSE <osmansalih717@gmail.com>
2018-12-24 22:00:33 -0500

    Datastructure update

--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,9 @@
 package datastructure;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,7 +14,21 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
-
+		Map<Integer, String> map = new HashMap<>();
+		map.put(1, "Yousif Ali");
+		map.put(2, "Marwa Zahid");
+		map.put(3, "Ahlam Abid");
+		map.put(4, "Zaynab Ali");
+		//using for each loop
+		for (Map.Entry entry : map.entrySet()) {
+			System.out.println( entry.getKey() + " -> " + entry.getValue() );
+		}
+		//using while
+		Iterator<Map.Entry<Integer, String>> entries = map.entrySet().iterator();
+		while (entries.hasNext()) {
+			Map.Entry<Integer, String> entry = entries.next();
+			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+		}
 	}
 
 }
--- src/datastructure/UseQueue.java
+++ src/datastructure/UseQueue.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class UseQueue {
 
 	public static void main(String[] args) {
@@ -8,6 +11,27 @@ public class UseQueue {
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * 
 		 */
+		// using add to add elements to queue
+		Queue q= new LinkedList();
+		q.add("1, Maryam");
+		q.add("2, Arifa");
+		q.add("3, Nafia");
+		q.add("4, Basima");
+		q.add("5, Siham");
+		q.add("6, Layla");
+		//iterate through the queue
+		for (Object o: q) {
+String element	 = (String) o;
+			System.out.println(o);
+		}
+		//using  peek to check the top element in the queue
+
+		Object top = q.peek();
+		System.out.println("Peek element is : "+top);
+		// using remove
+		((LinkedList) q).remove();
+		top = q.peek();
+		System.out.println("New Peek after remove is : "+top);
 
 	}
 
