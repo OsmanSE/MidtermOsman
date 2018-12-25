8c5502be2094e572c160d6bf2f409c7ac27a6870
OsmanSE <osmansalih717@gmail.com>
2018-12-24 23:43:56 -0500

    update

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,10 @@
 package datastructure;
 
+import javax.swing.text.html.HTMLDocument;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.ListIterator;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -9,8 +14,22 @@ public class UseArrayList {
 		 * Store all the sorted data into one of the databases.
 		 * 
 		 */
-	
 
+		ArrayList al= new ArrayList();
+		al.add("100   Layla Omar");
+		al.add("200   Jamal Adam");
+		al.add("400   Layla Samir");
+		al.add("300   Samah Badri");
+		Iterator it= al.listIterator();
+		while (it.hasNext()){
+			Object  o=it.next();
+			System.out.println(o);
+		}
+		System.out.println("\n*****************************\n");
+		for (Object o: al){
+
+			System.out.println(o);
+		}
 	}
 
 }
--- src/design/Employee.java
+++ src/design/Employee.java
@@ -24,3 +24,62 @@ public interface Employee {
 	public void benefitLayout();
 
 }
+
+class emp implements Employee{
+
+	              private String name;
+	              private int id;
+	               private int dno;
+	                 private int rate;
+	                private int sal;
+
+	public String    getName() {
+		return name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getDno() {
+		return dno;
+	}
+
+	public int getSal() {
+		return sal;
+	}
+
+	public int getRate() {
+		return rate;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setDno(int dno) {
+		this.dno = dno;
+	}
+
+	public void setRate(int rate) {
+		this.rate = rate;
+	}
+
+	public void setSal(int sal) {
+		this.sal = sal;
+	}
+
+	public String employeeName() {return "";}
+	public int employeeId(){ return 0;}
+
+	public int hours(int a){return a;}
+	public void assignDepartment()       {
+		System.out.println();
+	}
+	public int calculateSalary(){return 0;}
+	public void benefitLayout() {}
+}
